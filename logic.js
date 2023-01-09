3f7eab5de9aec9b1f0e0ea5b6fb51668136c0ba3
padmed <ddarchinian@gmail.com>
2023-01-10 01:08:01 +0400

    Make Quick transactions elements interactive on touchscreen

--- logic.js
+++ logic.js
@@ -10,3 +10,23 @@ var swiper = new Swiper(".swiper", {
     },
   },
 });
+
+window.addEventListener("touchstart", (event) => {
+  console.log(event.target.classList);
+  if (
+    event.target.classList.contains("photo-placeholder") ||
+    event.target.classList.contains("add-contact")
+  ) {
+    event.target.classList.add("active");
+  }
+});
+
+window.addEventListener("touchend", (event) => {
+  console.log(event.target.classList);
+  if (
+    event.target.classList.contains("photo-placeholder") ||
+    event.target.classList.contains("add-contact")
+  ) {
+    event.target.classList.remove("active");
+  }
+});
