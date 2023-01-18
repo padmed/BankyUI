c529d4eba60080ee170afb51723e13e5aedda90d
padmed <ddarchinian@gmail.com>
2023-01-18 22:53:44 +0400

    Add animation on opening and closing notifications

--- logic.js
+++ logic.js
@@ -1,3 +1,4 @@
+// Slider functionality for mobile design
 var swiper = new Swiper(".swiper", {
   grabCursor: true,
   effect: "creative",
@@ -11,6 +12,7 @@ var swiper = new Swiper(".swiper", {
   },
 });
 
+// Hover effect on contact buttons for mobile design
 window.addEventListener("touchstart", (event) => {
   console.log(event.target.classList);
   if (
@@ -28,5 +30,37 @@ window.addEventListener("touchend", (event) => {
     event.target.classList.contains("add-contact")
   ) {
     event.target.classList.remove("active");
+  }
+});
+
+//desktop functionalities
+const notificationContainer = document.querySelector("#notification-container");
+const notificationSetting = document.querySelector(".notifications");
+const envelopeIcon = document.querySelector("#notification");
+const envelopeIconOpened = document.querySelector("#notification-opened");
+
+function openEnvelope() {
+  envelopeIcon.classList.add("hidden");
+  envelopeIcon.classList.remove("display");
+
+  envelopeIconOpened.classList.add("display");
+  envelopeIconOpened.classList.remove("hidden");
+}
+
+function closeEnvelope() {
+  envelopeIconOpened.classList.add("hidden");
+  envelopeIconOpened.classList.remove("display");
+
+  envelopeIcon.classList.add("display");
+  envelopeIcon.classList.remove("hidden");
+}
+
+notificationSetting.addEventListener("click", (event) => {
+  notificationContainer.style.display = "grid";
+
+  if (event.target === envelopeIcon) {
+    openEnvelope();
+  } else if (event.target === envelopeIconOpened) {
+    closeEnvelope();
   }
 });
