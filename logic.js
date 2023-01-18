f960e1f5dd4712b5b8798dba428126bc335baa37
padmed <ddarchinian@gmail.com>
2023-01-18 22:56:55 +0400

    Change variable names, make notification visible on click

--- logic.js
+++ logic.js
@@ -39,28 +39,30 @@ const notificationSetting = document.querySelector(".notifications");
 const envelopeIcon = document.querySelector("#notification");
 const envelopeIconOpened = document.querySelector("#notification-opened");
 
-function openEnvelope() {
+function openNotification() {
   envelopeIcon.classList.add("hidden");
   envelopeIcon.classList.remove("display");
 
   envelopeIconOpened.classList.add("display");
   envelopeIconOpened.classList.remove("hidden");
+
+  notificationContainer.style.display = "grid";
 }
 
-function closeEnvelope() {
+function closeNotification() {
   envelopeIconOpened.classList.add("hidden");
   envelopeIconOpened.classList.remove("display");
 
   envelopeIcon.classList.add("display");
   envelopeIcon.classList.remove("hidden");
+
+  notificationContainer.style.display = "none";
 }
 
 notificationSetting.addEventListener("click", (event) => {
-  notificationContainer.style.display = "grid";
-
   if (event.target === envelopeIcon) {
-    openEnvelope();
+    openNotification();
   } else if (event.target === envelopeIconOpened) {
-    closeEnvelope();
+    closeNotification();
   }
 });
